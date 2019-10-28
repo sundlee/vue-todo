a1da1a4c6b5f25e20b0fcfdb02d5d7750e49966b
sundlee <sundlee@gmail.com>
2019-10-28 22:26:08 +0900

    initial commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" />
+    <span class="addContainer" @click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+	data: function() {
+		return {
+			newTodoItem: '',
+		};
+	},
+	methods: {
+		addTodo: function() {
+			if (this.newTodoItem !== '') {
+				var obj = {
+					completed: false,
+					item: this.newTodoItem,
+				};
+				localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+				this.clearInput();
+			}
+		},
+		clearInput: function() {
+			this.newTodoItem = '';
+		},
+	},
+};
+</script>
+
+<style scoped>
+input:focus {
+	outline: none;
+}
+.inputBox {
+	background: white;
+	height: 50px;
+	line-height: 50px;
+	border-radius: 5px;
+}
+.inputBox input {
+	border-style: none;
+	font-size: 0.9rem;
+}
+.addContainer {
+	float: right;
+	background: linear-gradient(to right, #6478fb, #8763fb);
+	display: block;
+	width: 3rem;
+	border-radius: 0 5px 5px 0;
+}
+.addBtn {
+	color: white;
+	vertical-align: middle;
+}
+.closeModalBtn {
+	color: #42b983;
+}
+</style>
