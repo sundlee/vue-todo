ad00531a563a1d9efcf96b1e84153efa156aef7a
sundlee <sundlee@gmail.com>
2019-11-16 09:41:54 +0900

    vuex 헬퍼 함수를 사용하도록 수정 + 스토어 getters, mutations를 모듈화

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,10 +5,10 @@
         <i
           class="checkBtn fas fa-check"
           :class="{checkBtnCompleted: todoItem.completed}"
-          @click="toggleComplete(todoItem, index)"
+          @click="toggleComplete({ todoItem, index })"
         ></i>
         <span :class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
-        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+        <span class="removeBtn" @click="removeTodo({ todoItem, index })">
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
@@ -17,15 +17,13 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 export default {
 	methods: {
-		removeTodo(todoItem, index) {
-			this.$store.commit('removeOneItem', { todoItem, index });
-		},
-		toggleComplete(todoItem, index) {
-			this.$store.commit('toggleOneItem', { todoItem, index });
-		},
+		...mapMutations({
+			removeTodo: 'removeOneItem',
+			toggleComplete: 'toggleOneItem',
+		}),
 	},
 	computed: {
 		...mapGetters(['storedTodoItems']),
